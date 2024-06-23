e370261fb3c9c12104d5e8ed98bc73adbb1323ee
p.yost <peter.yost@intive.com>
2024-06-23 17:51:24 +0200

    adjust endpoints

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+import time
+
 from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask_cors import cross_origin
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 @app.route('/startTimer', methods=['POST'])
+@cross_origin()
 def startTimer():  # put application's code here
-    print(request.get_json())
-    socketio.emit('timer_started', {'message': 'Timer has started!'})
+    tick = request.json.get('time')
+    while tick:
+        time.sleep(1)
+        tick -= 1
+        socketio.emit('time', tick)
+        print(tick)
+
+
     return "ok", 200
 
 
-@app.route('/weight', methods=['POST'])
-def weight():
-    print(request.get_json())
+@app.route('/values', methods=['POST'])
+@cross_origin()
+def values():
+    # print(request.json)
     return "ok", 200
 
 @app.route('/valid', methods=['POST'])
