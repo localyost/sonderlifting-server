8f1784e68d4a0fdb4f68af191dcc01428f333438
p.yost <peter.yost@intive.com>
2024-06-25 16:16:40 +0200

    move countdown to server

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request
 from flask_cors import cross_origin
 from flask_socketio import SocketIO
@@ -6,12 +8,17 @@ app = Flask(__name__)
 
 socketio = SocketIO(app, debug=True, cors_allowed_origins='*', use_reloader=False)
 
+
 @app.route('/startTimer', methods=['POST'])
 @cross_origin()
 def startTimer():
-
+    duration = request.json.get('time')
     socketio.emit('VALUES', request.json)
-    socketio.emit('TIMER_START', request.json.get('time'))
+    socketio.emit('VALID')
+    while duration >= 0:
+        socketio.emit('TIME', duration)
+        time.sleep(1)
+        duration -= 1
     return "ok", 200
 
 
@@ -21,11 +28,18 @@ def values():
     socketio.emit('VALUES', request.json)
     return "ok", 200
 
+
 @app.route('/valid', methods=['POST'])
 @cross_origin()
 def valid():
-    socketio.emit('TIMER_STOP')
     socketio.emit('VALID', request.json.get('valid'))
+    return "ok", 200
+
+
+@app.route('/clear', methods=['POST'])
+@cross_origin()
+def clear():
+    socketio.emit('VALID')
     return "ok", 200
 
 
